ccb53e7a05ac1fda8e3c7219011f139e1519174d
Sergei Kraev <serega.kraev1@yandex.ru>
2025-04-15 14:57:07 +0300

    initial commit

---
+++ src/main/java/ru/kraser/technical_helper/TechnicalHelperApplication.java
@@ -0,0 +1,13 @@
+package ru.kraser.technical_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kraser/technical_helper/TechnicalHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kraser.technical_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
