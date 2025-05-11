b3cb12b735ce999283684d17760997de7a34cb75
Sergei Kraev <serega.kraev1@yandex.ru>
2025-05-11 15:34:19 +0300

    add AuthException()

---
+++ common-module/src/main/java/ru/kraser/technical_helper/common_module/exception/AuthException.java
@@ -0,0 +1,8 @@
+package ru.kraser.technical_helper.common_module.exception;
+
+public class AuthException extends RuntimeException {
+
+    public AuthException(String message) {
+        super(message);
+    }
+}
--- main-server/src/main/java/ru/kraser/technical_helper/main_server/security/AuthenticationService.java
+++ main-server/src/main/java/ru/kraser/technical_helper/main_server/security/AuthenticationService.java
@@ -3,10 +3,12 @@ package ru.kraser.technical_helper.main_server.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 import ru.kraser.technical_helper.common_module.dto.auth.AuthenticationRequest;
 import ru.kraser.technical_helper.common_module.dto.auth.AuthenticationResponse;
 import ru.kraser.technical_helper.common_module.exception.NotFoundException;
+import ru.kraser.technical_helper.common_module.exception.AuthException;
 import ru.kraser.technical_helper.main_server.repository.UserRepository;
 
 @Service
@@ -17,12 +19,16 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.username(),
-                        request.password()
-                )
-        );
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.username(),
+                            request.password()
+                    )
+            );
+        } catch (AuthenticationException e) {
+            throw new AuthException("Неверное имя пользователя или пароль !!!");
+        }
         var user = userRepository.findUserByUsernameAndEnabledTrue(request.username()).orElseThrow(
                 () -> new NotFoundException("Пользователь с логином - " + request.username() + ", не был найден.")
         );
