0a7ca2dc1f14bb64ee3e9f2b1824d965f0446d04
Sergei Kraev <serega.kraev1@yandex.ru>
2025-07-11 11:19:10 +0300

    add Dockerfiles

---
+++ common-module/src/main/java/ru/kraser/technical_helper/CommonModule.java
@@ -0,0 +1,11 @@
+package ru.kraser.technical_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonModule {
+    public static void main(String[] args) {
+        SpringApplication.run(CommonModule.class, args);
+    }
+}
