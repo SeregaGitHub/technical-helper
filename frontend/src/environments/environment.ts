9875b8df92f9913f23980f0d045365cab471dc30
Sergei Kraev <serega.kraev1@yandex.ru>
2025-07-17 17:51:41 +0300

    delete some comments

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
-    // GATEWAY_URL: 'http://192.168.0.102:12344',
-    GATEWAY_URL: 'http://gateway:12344'
+    GATEWAY_URL: 'http://192.168.0.102:12344',
 }
