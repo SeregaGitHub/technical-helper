a90a4d8777d565e13bbb8bfd2a5c34398f0d6667
Sergei Kraev <serega.kraev1@yandex.ru>
2025-08-06 17:20:02 +0300

    update BaseEntity.class

--- common-module/src/main/java/ru/kraser/technical_helper/common_module/model/BaseEntityIdShot.java
+++ common-module/src/main/java/ru/kraser/technical_helper/common_module/model/BaseEntityIdShot.java
@@ -4,21 +4,22 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.UuidGenerator;
-
-import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class BaseEntity extends BaseEntityIdShot {
+public class BaseEntityIdShot extends BaseEntityShot {
 
-    @Column(name = "created_by", length = 36)
-    public String createdBy;
-
-    @Column(name = "created_date")
-    public LocalDateTime createdDate;
+    @Id
+    @GeneratedValue
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
+    @Column(name = "id", length = 36, nullable = false)
+    public String id;
 }
