6931057128f1d35b50c458d574baa2c7d2f7ca09
Sergei Kraev <serega.kraev1@yandex.ru>
2025-08-15 00:50:03 +0300

    add logs to Department server and gateway

--- main-server/src/main/java/ru/kraser/technical_helper/main_server/controller/DepartmentController.java
+++ main-server/src/main/java/ru/kraser/technical_helper/main_server/controller/DepartmentController.java
@@ -1,6 +1,7 @@
 package ru.kraser.technical_helper.main_server.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.kraser.technical_helper.common_module.dto.api.ApiResponse;
@@ -14,6 +15,7 @@ import static ru.kraser.technical_helper.common_module.util.Constant.*;
 
 @RestController
 @RequestMapping(path = BASE_URL + ADMIN_URL + DEPARTMENT_URL)
+@Slf4j
 @RequiredArgsConstructor
 public class DepartmentController {
     private final DepartmentService departmentService;
@@ -21,31 +23,46 @@ public class DepartmentController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse createDepartment(@RequestBody CreateDepartmentDto createDepartmentDto) {
-        return departmentService.createDepartment(createDepartmentDto);
+        log.info("Creating Department with name - {}", createDepartmentDto.name());
+        ApiResponse apiResponse = departmentService.createDepartment(createDepartmentDto);
+        log.info("Department with name - {}, successfully created", createDepartmentDto.name());
+        return apiResponse;
     }
 
     @PatchMapping
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse updateDepartment(@RequestHeader (DEPARTMENT_ID_HEADER) String departmentId,
                                    @RequestBody CreateDepartmentDto departmentDto) {
-        return departmentService.updateDepartment(departmentId, departmentDto);
+        log.info("Updating Department with Id={}", departmentId);
+        ApiResponse apiResponse =  departmentService.updateDepartment(departmentId, departmentDto);
+        log.info("Department with Id={}, successfully updated", departmentId);
+        return apiResponse;
     }
 
     @GetMapping(path = ALL_URL)
     @ResponseStatus(HttpStatus.OK)
     public List<DepartmentDto> getAllDepartments() {
-        return departmentService.getAllDepartments();
+        log.info("Getting all Departments");
+        List<DepartmentDto> departments =  departmentService.getAllDepartments();
+        log.info("All Departments received successfully");
+        return departments;
     }
 
     @GetMapping(path = CURRENT_URL)
     @ResponseStatus(HttpStatus.OK)
     public DepartmentDto getDepartment(@RequestHeader (DEPARTMENT_NAME_HEADER) String departmentName) {
-        return departmentService.getDepartment(departmentName);
+        log.info("Getting Department with name - {}", departmentName);
+        DepartmentDto department =  departmentService.getDepartment(departmentName);
+        log.info("Department with name - {}, received successfully", departmentName);
+        return department;
     }
 
     @PatchMapping(path = DELETE_URL)
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse deleteDepartment(@RequestHeader (DEPARTMENT_ID_HEADER) String departmentId) {
-        return departmentService.deleteDepartment(departmentId);
+        log.info("Deleting Department with Id={}", departmentId);
+        ApiResponse apiResponse =  departmentService.deleteDepartment(departmentId);
+        log.info("Department with Id={}, successfully deleted", departmentId);
+        return apiResponse;
     }
 }
