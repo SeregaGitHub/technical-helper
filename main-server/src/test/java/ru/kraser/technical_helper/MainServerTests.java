728496c20ae21b3c7fc62b9fee7b363d6a260e93
Sergei Kraev <serega.kraev1@yandex.ru>
2026-02-19 12:37:43 +0300

    add classes = MainServer.class

--- main-server/src/test/java/ru/kraser/technical_helper/MainServerTests.java
+++ main-server/src/test/java/ru/kraser/technical_helper/MainServerTests.java
@@ -3,7 +3,7 @@ package ru.kraser.technical_helper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MainServer.class)
 public class MainServerTests {
 
     @Test
