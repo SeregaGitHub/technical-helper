f7e15e8b84baa2a34f21cb66ae8ccf5676e11b06
Sergei Kraev <serega.kraev1@yandex.ru>
2026-02-19 12:35:11 +0300

    rebase MainServerTests.class

--- main-server/src/test/java/ru/kraser/technical_helper/MainServerTests.java
+++ main-server/src/test/java/ru/kraser/technical_helper/MainServerTests.java
@@ -1,4 +1,4 @@
-package ru.kraser.technical_helper.main_server;
+package ru.kraser.technical_helper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
