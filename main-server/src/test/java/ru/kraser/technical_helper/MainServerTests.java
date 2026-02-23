219ec47d218828563894e76b768c99e1661eba4e
Sergei Kraev <serega.kraev1@yandex.ru>
2026-02-23 17:09:21 +0300

    add DynamicPropertySource

--- main-server/src/test/java/ru/kraser/technical_helper/MainServerTests.java
+++ main-server/src/test/java/ru/kraser/technical_helper/MainServerTests.java
@@ -3,6 +3,8 @@ package ru.kraser.technical_helper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -11,9 +13,25 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @SpringBootTest(classes = MainServer.class)
 public class MainServerTests {
 
-    @Container
+    /*@Container
     @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine");
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine");*/
+
+    @Container
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(
+            "postgres:17-alpine")
+            .withDatabaseName("technical_helper")
+            .withUsername("sa")
+            .withPassword("sapassword")
+            .withInitScript("schema.sql");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
 
     @Test
     void contextLoads() {
